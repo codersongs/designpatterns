bf1d215b426c8c310c6305afc02fc6b5cce804e6
codersongs <codersongs@163.com>
2018-04-01 22:48:30 +0800

    删除MVC

--- src/main/java/com/codersongs/framepattern/mvc/StudentView.java
+++
@@ -1,8 +0,0 @@
-package com.codersongs.framepattern.mvc;
-
-public class StudentView {
-    public void printStudentDetails(String name, String realNo){
-        System.out.println("Student:");
-        System.out.println("name:" + name + "\t" + "RealNO:" + realNo);
-    }
-}
